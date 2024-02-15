38b73ccfd134c1c2981109d9d3e663b980e7f629
Evan Donahue <emdonahu@gmail.com>
2024-02-15 13:12:07 +0900

    set up basic testing methods

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
+let logging = false;
+function log() {
+    if (logging) console.log(arguments);
+}
+
 function test(f, test_name) {
     try {
 	f();
@@ -5,6 +10,17 @@ function test(f, test_name) {
 	document.write('error in test: ' + (test_name || 'Unnamed'));
 	throw x;
     }
+}
+
+function assert(a, ...debug_info) {
+    if (!a) {
+	console.log(...debug_info);
+	throw Error("Assertion failed");
+    }
+}
+
+function equals(a, b) {
+    return JSON.stringify(a) == JSON.stringify(b);
 }
 
 function asserte(a, b) {
@@ -15,6 +31,14 @@ function asserte(a, b) {
 class LVar {
     constructor(id) {
 	this.id = id;
+    }
+}
+
+class PropObserver {
+    constructor(node, attr, val) {
+	this.node = node;
+	this.attr = attr;
+	this.val = val;
     }
 }
 
@@ -59,21 +83,30 @@ function normalize(model, substitution) {
     }
 }
 
-let subst = [];
-let m = normalize({
-    a: 1,
-    b: 2,
-    c: [3, 4],
-    d: {e: 5, f: 6}
-}, subst);
-console.log(subst);
+function walk(substitution, lvar) {
+    if (!(lvar instanceof LVar)) return lvar;
+	return substitution[lvar.id];
+}
 
-function render(spec) {
+
+// RENDERING
+
+function render(spec, sub, observers) {
+    log('render', spec, sub, observers);
+    assert(sub);
     if (typeof spec == 'string' || typeof spec == 'number') return document.createTextNode(spec);
     else if (Array.isArray(spec)) {
 	let parent = document.createElement(spec[0]);
-	for (let i=1; i<spec.length; i++) parent.appendChild(render(spec[i]));
+	for (let i=1; i<spec.length; i++) {
+	    log('child render', render(spec[i], sub, observers));
+	    parent.appendChild(render(spec[i], sub, observers));
+	}
 	return parent;
+    }
+    else if (spec instanceof LVar) {
+	return render(walk(sub, spec), sub, observers);
+	
+	
     }
     else throw Error('Unrecognized render spec: ' + JSON.stringify(spec));
     //    		typeof child === 'number') head.appendChild(document.createTextNode(child));
@@ -81,4 +114,21 @@ function render(spec) {
 }
 
 
-console.log(render(['div', 'this is a test']));
+// TESTING
+
+let s = [];
+let m = normalize({
+    a: 1,
+    b: 2,
+    c: [3, 4],
+    d: {e: 5, f: 6}
+}, s);
+
+
+
+
+log("model",m);
+log("substitution",s);
+
+assert(walk(s, m).a instanceof LVar, walk(s, m).a);
+asserte(render(walk(s,m).a, s).textContent, '1');
